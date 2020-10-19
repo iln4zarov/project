ae487f26c528dd276644c888fe8e0ce061c6ca1a
Artem41k <waw0905@mail.ru>
2020-10-19 11:50:24 +0500

    Homework 3

---
+++ HW3/Homework.js
@@ -0,0 +1,14 @@
+function testStr(a, b) {
+     a = a.toUpperCase();
+     b = b.toLowerCase();
+    console.log(a + b);
+}
+
+testStr("Hello", "World!");
+
+function testStr2(c, d) {
+    console.log(c.indexOf(d));
+}
+
+testStr2("This is a test string", "test")
+
